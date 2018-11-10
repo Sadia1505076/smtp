2f4ef2b5340000a221fb0b31e68a6e29f5d9f821
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2018-11-10 08:48:58 +0600

    done

--- SMTPClient/src/smtpclient/SMTPClient.java
+++ SMTPClient/src/smtpclient/SMTPClient.java
@@ -13,11 +13,16 @@ import java.util.Scanner;
 public class SMTPClient {
 
     public static void main(String[] args) throws UnknownHostException, IOException {
+//        String mailServer = "webmail.buet.ac.bd";
+//        InetAddress mailHost = InetAddress.getByName(mailServer);
+//        //InetAddress mailHost = InetAddress.getLocalHost();
+//        InetAddress localHost = InetAddress.getLocalHost();
+//        Socket smtpSocket = new Socket(mailHost, 25);
+
         String mailServer = "webmail.buet.ac.bd";
-        //InetAddress mailHost = InetAddress.getByName(mailServer);
-        InetAddress mailHost = InetAddress.getLocalHost();
+        InetAddress mailHost = InetAddress.getByName(mailServer);
         InetAddress localHost = InetAddress.getLocalHost();
-        Socket smtpSocket = new Socket(mailHost, 25);
+        Socket smtpSocket = new Socket(mailHost,25);
         smtpSocket.setSoTimeout(20000);
         BufferedReader in = new BufferedReader(new InputStreamReader(smtpSocket.getInputStream()));
         PrintWriter pr = new PrintWriter(smtpSocket.getOutputStream(), true);
